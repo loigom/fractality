97458ddb76002e37b624ec60d32f26749b5379c1
lethargiousgithub <mothrakk1@gmail.com>
2020-04-10 22:44:12 +0300

    zooming functionality

--- main.js
+++ main.js
@@ -1,27 +1,58 @@
-/* Canvas related things */
-const canvas = document.getElementById("canv");
-const ctx = canvas.getContext("2d");
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
-/* ------------------- */
-
 /* Things related to rendering */
-const MAX_ITER = 80 // Amount of "passes" to do during rendering. Higher -> better fractal precision.
-const MOD_CONST = 2 // Variable to determine if a pixel/complex number has escaped.
+let MAX_ITER = 35 // Amount of "passes" to do during rendering. Higher -> better fractal detail.
+const MOD_CONST = 4 // Variable to determine if a pixel/complex number has escaped.
 
-const RE_START = -2 // These 4 are for determining which "place" in the fractal should get drawn on a x-y axis.
-const RE_END = 1
-const IM_START = -1
-const IM_END = 1
+
+let RE_START = -2 // These 4 are for determining the boundaries.
+let RE_END = 1
+let IM_START = -1
+let IM_END = 1
+
+let RE_START_NEW;
+let RE_END_NEW;
+let IM_START_NEW;
+let IM_END_NEW;
+
 /* ------------------- */
 
 /* These variables are for thread control so the entire window isn't blocked until the entire fractal is finished rendering. */
-let RENDERING = false;
+let RENDERING = true;
+const DRAW_TICKRATE = 1;
 const ROWS_PER_TICK = 8; /* This determines how many rows get rendered before the thread control is released back to the browser for drawing on screen.
                             A higher amount generally means faster rendering, but the process looks more choppy. */
 let y = 0; // The y-coordinate used in draw()
 /* ------------------- */
 
+/* Some colors. */
+const BLACK = [0, 0, 0];
+/* ------------------- */
+
+/* Canvas related things */
+const canvas = document.getElementById("canv");
+const ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.onmousedown = function(e) {
+    [RE_START_NEW, IM_START_NEW] = screenToComplexCoords(e.clientX, e.clientY);
+}
+canvas.onmouseup = function(e) {
+    [RE_END, IM_END] = screenToComplexCoords(e.clientX, e.clientY);
+    RE_START = RE_START_NEW;
+    IM_START = IM_START_NEW;
+    MAX_ITER *= 10;
+    if (MAX_ITER > 7000) {
+        MAX_ITER = 7000;
+    }
+    y = 0;
+    RENDERING = true;
+}
+/* ------------------- */
+
+function screenToComplexCoords(x, y) {
+    let realPerPixel = Math.abs(RE_START - RE_END) / window.innerWidth;
+    let imagPerPixel = Math.abs(IM_START - IM_END) / window.innerHeight;
+    return [realPerPixel * x + RE_START, imagPerPixel * y + IM_START];
+}
 
 function setPixel(x, y, r, g, b) {
     ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
@@ -31,7 +62,7 @@ function setPixel(x, y, r, g, b) {
 // This is just some greyscale color pallette I made up. Looks okay-ish.
 function setRBG(iters) {
     let quotinent = iters / MAX_ITER;
-    if (quotinent > 0.9) { return [0, 0, 0]; }
+    if (quotinent > 0.9) { return BLACK; }
     let rgb = Math.floor((255 / MAX_ITER) * iters);
     return [rgb, rgb, rgb];
 }
@@ -44,24 +75,21 @@ function draw() {
         }
         else {
             let x, real, imag, iters, r, g, b;
-            let i = 0;
-            while (y < canvas.height && i < ROWS_PER_TICK) {
+            let i = ROWS_PER_TICK;
+            let realDistance = RE_END - RE_START;
+            let imagDistance = IM_END - IM_START;
+            while (y < canvas.height && i--) {
                 for (x = 0; x < canvas.width; x++) {
-                    real = RE_START + (x / canvas.width) * (RE_END - RE_START);
-                    imag = IM_START + (y / canvas.height) * (IM_END - IM_START);
+                    real = RE_START + (x / canvas.width) * realDistance;
+                    imag = IM_START + (y / canvas.height) * imagDistance;
                     iters = mandelbrot(real, imag);
                     [r, g, b] = setRBG(iters);
                     setPixel(x, y, r, g, b);
                 }
                 y++;
-                i++;
             }
         }
     }
-}
-
-function complex_abs(real, imag) {
-    return Math.sqrt(real*real + imag*imag);
 }
 
 function complex_squared(real, imag) {
@@ -72,7 +100,7 @@ function complex_squared(real, imag) {
 
 function mandelbrot(real_constant, imag_constant) {
     let iters = real = imag = 0;
-    while (complex_abs(real, imag) <= MOD_CONST && iters < MAX_ITER) {
+    while ((real*real + imag*imag) <= MOD_CONST && iters < MAX_ITER) {
         [real, imag] = complex_squared(real, imag);
         real += real_constant;
         imag += imag_constant;
@@ -81,5 +109,4 @@ function mandelbrot(real_constant, imag_constant) {
     return iters;
 }
 
-document.getElementById("drawBtn").onclick = function() { RENDERING = true; }
-setInterval(draw, 15); // Tick draw() every 15ms.
+setInterval(draw, DRAW_TICKRATE);
