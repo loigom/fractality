f8c24b7f186de95482f5206e4f999f6129a0ace4
lethargiousgithub <mothrakk1@gmail.com>
2020-04-10 20:09:27 +0300

    now shows progress in rendering the fractal

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
-let canvas = document.getElementById("canv");
-let ctx = canvas.getContext("2d");
+/* Canvas related things */
+const canvas = document.getElementById("canv");
+const ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+/* ------------------- */
 
-MAX_ITER = 80
-MOD_CONST = 2
+/* Things related to rendering */
+const MAX_ITER = 80 // Amount of "passes" to do during rendering. Higher -> better fractal precision.
+const MOD_CONST = 2 // Variable to determine if a pixel/complex number has escaped.
 
-RE_START = -2
-RE_END = 1
-IM_START = -1
-IM_END = 1
+const RE_START = -2 // These 4 are for determining which "place" in the fractal should get drawn on a x-y axis.
+const RE_END = 1
+const IM_START = -1
+const IM_END = 1
+/* ------------------- */
+
+/* These variables are for thread control so the entire window isn't blocked until the entire fractal is finished rendering. */
+let RENDERING = false;
+const ROWS_PER_TICK = 8; /* This determines how many rows get rendered before the thread control is released back to the browser for drawing on screen.
+                            A higher amount generally means faster rendering, but the process looks more choppy. */
+let y = 0; // The y-coordinate used in draw()
+/* ------------------- */
+
 
 function setPixel(x, y, r, g, b) {
     ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
     ctx.fillRect(x, y, 1, 1);
 }
 
+// This is just some greyscale color pallette I made up. Looks okay-ish.
 function setRBG(iters) {
     let quotinent = iters / MAX_ITER;
     if (quotinent > 0.9) { return [0, 0, 0]; }
@@ -22,16 +37,25 @@ function setRBG(iters) {
 }
 
 function draw() {
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-    let x, y, real, imag, iters, r, g, b;
-    for (x = 0; x < canvas.width; x++) {
-        for (y = 0; y < canvas.height; y++) {
-            real = RE_START + (x / canvas.width) * (RE_END - RE_START);
-            imag = IM_START + (y / canvas.height) * (IM_END - IM_START);
-            iters = mandelbrot(real, imag);
-            [r, g, b] = setRBG(iters);
-            setPixel(x, y, r, g, b);
+    if (RENDERING) {
+        if (y == canvas.height) {
+            RENDERING = false;
+            y = 0;
+        }
+        else {
+            let x, real, imag, iters, r, g, b;
+            let i = 0;
+            while (y < canvas.height && i < ROWS_PER_TICK) {
+                for (x = 0; x < canvas.width; x++) {
+                    real = RE_START + (x / canvas.width) * (RE_END - RE_START);
+                    imag = IM_START + (y / canvas.height) * (IM_END - IM_START);
+                    iters = mandelbrot(real, imag);
+                    [r, g, b] = setRBG(iters);
+                    setPixel(x, y, r, g, b);
+                }
+                y++;
+                i++;
+            }
         }
     }
 }
@@ -57,4 +81,5 @@ function mandelbrot(real_constant, imag_constant) {
     return iters;
 }
 
-draw();
+document.getElementById("drawBtn").onclick = function() { RENDERING = true; }
+setInterval(draw, 15); // Tick draw() every 15ms.
